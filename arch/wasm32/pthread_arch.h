88e53f3c9a83d18665f3c386df93f3d011e05e74
Simon Kadisch <simon.kadisch@k13-engineering.at>
2023-06-17 11:41:29 +0200

    Temporarly implement __get_tp() via get_thread_area()

--- arch/wasm32/pthread_arch.h
+++ arch/wasm32/pthread_arch.h
@@ -1,12 +1,33 @@
+
+struct user_desc {
+	unsigned int  entry_number;
+	unsigned int  base_addr;
+	unsigned int  limit;
+	unsigned int  seg_32bit:1;
+	unsigned int  contents:2;
+	unsigned int  read_exec_only:1;
+	unsigned int  limit_in_pages:1;
+	unsigned int  seg_not_present:1;
+	unsigned int  useable:1;
+};
+
 static inline uintptr_t __get_tp()
 {
 	// uintptr_t tp;
 	// __asm__ ("movl %%gs:0,%0" : "=r" (tp) );
 	// return tp;
 
-	__asm__("unreachable");
+	// __asm__("unreachable");
+	// return 0;
 
-	return 0;
+	struct user_desc desc;
+
+	int ret = __syscall(SYS_get_thread_area, &desc);
+	if (ret != 0) {
+		__asm__("unreachable");
+	}
+
+	return desc.base_addr;
 }
 
 #define MC_PC gregs[REG_EIP]
